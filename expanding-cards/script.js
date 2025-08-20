40aeba54551ea604fc0b51b2162c5a14f897d540
ekohadipermana <ekohadipermana@gmail.com>
2025-08-20 12:56:01 +0700

    cleaning up the js

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,19 +1,18 @@
 const cards = document.querySelectorAll('.card');
 
-    // Add a click event listener to each card
+// Add a click event listener to each card
+// Find the currently active card
+// document.querySelector() finds the first element that matches the selector
+// Check if an active card was found and remove the 'active' class
+// Add the 'active' class to the card that was just clicked
+// 'this' inside an event listener refers to the element that triggered the event
+
 cards.forEach(card => {
     card.addEventListener('click', () => {
-    // Find the currently active card
-    // document.querySelector() finds the first element that matches the selector
     const currentActiveCard = document.querySelector('.card.active');
-
-    // Check if an active card was found and remove the 'active' class
     if (currentActiveCard) {
         currentActiveCard.classList.remove('active');
-    }
-    
-    // Add the 'active' class to the card that was just clicked
-    // 'this' inside an event listener refers to the element that triggered the event
+    }        
     card.classList.add('active');
     });
 });
